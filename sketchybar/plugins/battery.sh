7347c88dcff4f6e115a1ca92cdd17d8af9b36119
Marcus Klingberg <marcus.klingberg@walley.se>
2024-03-31 14:16:19 +0200

    Moved battery.sh to plugins directory

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
+
+COLOR_BATTERY_LOW=0xffee99a0
+COLOR_BATTERY_EMPTY=0xffed8796
+
 source "$CONFIG_DIR/colors.sh" # Loads all defined colors
+
 # Battery is here bcause the ICON_COLOR doesn't play well with all background colors
 
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
@@ -12,29 +17,29 @@ fi
 case ${PERCENTAGE} in
 [8-9][0-9] | 100)
     ICON=""
-    ICON_COLOR=$TEXT_COLOR
+    ICON_COLOR=$COLOR_TEXT
     ;;
 7[0-9])
     ICON=""
-    ICON_COLOR=$TEXT_COLOR
+    ICON_COLOR=$COLOR_TEXT
     ;;
 [4-6][0-9])
     ICON=""
-    ICON_COLOR=$SECONDARY_COLOR
+    ICON_COLOR=$COLOR_SECONDARY
     ;;
 [1-3][0-9])
     ICON=""
-    ICON_COLOR=0xffee99a0
+    ICON_COLOR=$COLOR_BATTERY_LOW
     ;;
 [0-9])
     ICON=""
-    ICON_COLOR=0xffed8796
+    ICON_COLOR=$COLOR_BATTERY_EMPTY
     ;;
 esac
 
 if [[ $CHARGING != "" ]]; then
     ICON=""
-    ICON_COLOR=$TEXT_COLOR
+    ICON_COLOR=$COLOR_TEXT
 fi
 
 sketchybar --set $NAME \
