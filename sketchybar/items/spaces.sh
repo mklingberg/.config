d4dc55086fb31e078c393f73186efd63e4b5f4c9
Marcus Klingberg <marcus.klingberg@walley.se>
2024-04-04 21:42:35 +0200

    Adding > marker < to selected space

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -4,19 +4,40 @@ SPACE_SIDS=(1 2 3 4 5 6 7 8 9 10)
 
 for sid in "${SPACE_SIDS[@]}"
 do
-  $BAR_NAME --add space space.$sid left                                 \
-             --set space.$sid space=$sid                                 \
-                              icon=$sid                                  \
-                              icon.padding_right=10                     \
-                              label.font="sketchybar-app-font:Regular:15.0" \
-                              label.padding_left=0                     \
-                              label.y_offset=-1                          \
-                              script="$PLUGIN_SHARED_DIR/space.sh"
+  $BAR_NAME \
+    --add item space.$sid.s left \
+    --set space.$sid.s \
+            icon= \
+            icon.color=$COLOR_SECONDARY \
+            background.color=$COLOR_SECONDARY \
+            icon.font="$FONT_FACE:Bold:$BAR_HEIGHT.0" \
+            icon.padding_left=0 \
+            icon.padding_right=-8 \
+            icon.y_offset=1  \
+     --add space space.$sid left \
+     --set space.$sid \
+                  space=$sid \
+                  icon=$sid \
+                  icon.padding_right=0 \
+                  label.font="sketchybar-app-font:Regular:15.0" \
+                  label.padding_left=0 \
+                  label.y_offset=-1 \
+                  script="$PLUGIN_SHARED_DIR/space.sh" \
+            --subscribe space.$sid space_change mouse.clicked \
+    --add item space.$sid.e left \
+    --set space.$sid.e \
+            icon= \
+            icon.color=$COLOR_SECONDARY \
+            background.color=$COLOR_SECONDARY \
+            icon.font="$FONT_FACE:Bold:$BAR_HEIGHT.0" \
+            icon.padding_left=0 \
+            icon.padding_right=-8 \
+            icon.y_offset=1
 done
 
-$BAR_NAME --add item space_separator left                             \
-           --set space_separator \
-                                 label.drawing=off                     \
-                                 icon.drawing=off                     \
-                                 script="$PLUGIN_SHARED_DIR/space_windows.sh" \
-           --subscribe space_separator space_windows_change             
+$BAR_NAME --add item space_separator left \
+          --set space_separator \
+                label.drawing=off \
+                icon.drawing=off \
+                script="$PLUGIN_SHARED_DIR/space_windows.sh" \
+          --subscribe space_separator space_windows_change             
--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -6,17 +6,27 @@
 source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
 
 if [ $SELECTED = true ]; then
-  $BAR_NAME --set $NAME background.drawing=on \
+  $BAR_NAME \
+            --set $NAME.s background.color=$COLOR_TEXT_CONTRAST \
+            --set $NAME.e background.color=$COLOR_TEXT_CONTRAST \
+            --set $NAME background.drawing=on \
                          background.color=$COLOR_TEXT_CONTRAST \
                          label.color=$COLOR_BLACK \
                          icon.color=$COLOR_BLACK \
-                         icon.padding_left=30 \
-                         label.padding_right=40
+                         icon.padding_left=20 \
+                         icon.padding_right=5 \
+                         label.padding_right=30 \
+                         label.drawing=on
 else
-  $BAR_NAME --set $NAME background.drawing=on \
+  $BAR_NAME \
+            --set $NAME.s background.color=$COLOR_SECONDARY \
+            --set $NAME.e background.color=$COLOR_SECONDARY \
+            --set $NAME background.drawing=on \
                          label.color=$COLOR_TEXT_CONTRAST \
                          icon.color=$COLOR_ACCENT \
                          background.color=$COLOR_SECONDARY \
-                         icon.padding_left=20 \
-                         label.padding_right=40
+                         icon.padding_left=10 \
+                         icon.padding_right=5 \
+                         label.padding_right=20 \
+                         label.drawing=on
 fi
