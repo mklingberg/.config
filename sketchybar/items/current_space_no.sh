72c881c43b68ef76f84aa654ee5a0aeae8504511
Marcus Klingberg <marcus.klingberg@walley.se>
2024-06-16 00:07:18 +0200

    Updated layout for laptop mode

--- sketchybar/items/current_space_no.sh
+++ sketchybar/items/current_space_no.sh
@@ -4,8 +4,8 @@ $BAR_NAME \
     --add item current_space left \
     --set current_space \
             icon.padding_right=10 \
-            icon.color=$COLOR_TEXT_CONTRAST \
-            background.color=$COLOR_SECONDARY \
+            icon.color=$COLOR_SPACE_SPACE_NO \
+            background.color=$COLOR_SPACE_BG \
             label.drawing=off \
             script="$PLUGIN_SHARED_DIR/current_space.sh" \
     --subscribe current_space space_change mouse.clicked
