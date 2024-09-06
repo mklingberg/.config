399f377205b563de6fb71b9fd448e042a1237fa8
Marcus Klingberg <marcus.klingberg@gmail.com>
2024-09-06 21:36:31 +0200

    Adding same color scale to volume as cpu usage.

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
+COLOR_VOLUME_LOW=0xffee99a0
+COLOR_VOLUME_MEDIUM=0xffed8796
+COLOR_VOLUME_HIGH=0xffff0000
+
+source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
 
 SHOW_LABEL=true
 ICON_PADDING_RIGHT=10
+ICON_COLOR=$COLOR_VOLUME_LOW  # Default color
 
 case ${INFO} in
 0)
     ICON="󰖁"
     SHOW_LABEL=false
     ;;
-[1-9]|[1-3][0-9])
+[1-9]|[1-4][0-9])
     ICON="󰖀"
     SHOW_LABEL=false
     ;;
-[4-6][0-9])
+[5-7][0-9])
     ICON="󰕾"
-    SHOW_LABEL=false
+    #SHOW_LABEL=false
+    ICON_COLOR=$COLOR_VOLUME_MEDIUM
     ;;
-[7-9][0-9]|100)
+[8-9][0-9]|100)
     ICON=""
+    ICON_COLOR=$COLOR_VOLUME_HIGH
     ;;
 esac
 
-$BAR_NAME --set $NAME icon=$ICON icon.padding_right=$ICON_PADDING_RIGHT label="$INFO%" label.drawing=$SHOW_LABEL
+$BAR_NAME --set $NAME icon=$ICON icon.padding_right=$ICON_PADDING_RIGHT label="$INFO%" label.drawing=$SHOW_LABEL label.color=$ICON_COLOR icon.color=$ICON_COLOR
