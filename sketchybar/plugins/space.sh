f569c0578ae78c98b5430280ab86ac77332dcabb
Marcus Klingberg <marcus.klingberg@gmail.com>
2024-09-10 21:55:44 +0200

    Padding, margin updates for desktop layout

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -5,15 +5,18 @@
 # https://felixkratz.github.io/sketchybar/config/components#space----associate-mission-control-spaces-with-an-item
 source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
 
+PADDING_RIGHT=30
+PADDING_LEFT=10
+
 if [ $SELECTED = true ]; then
   #background.color=$COLOR_SPACE_SELECTED_BG \
   $BAR_NAME \
             --set $NAME background.drawing=on \
                          label.color=$COLOR_SPACE_SELECTED_ICON \
                          icon.color=$COLOR_SPACE_SELECTED_SPACE_NO \
-                         icon.padding_left=10 \
+                         icon.padding_left=$PADDING_LEFT \
                          icon.padding_right=5 \
-                         label.padding_right=30 \
+                         label.padding_right=$PADDING_RIGHT \
                          label.drawing=on
 else
   $BAR_NAME \
@@ -21,8 +24,8 @@ else
                          label.color=$COLOR_SPACE_ICON \
                          icon.color=$COLOR_SPACE_SPACE_NO \
                          background.color=$TRANSPARENT \
-                         icon.padding_left=10 \
+                         icon.padding_left=$PADDING_LEFT \
                          icon.padding_right=5 \
-                         label.padding_right=30 \
+                         label.padding_right=$PADDING_RIGHT \
                          label.drawing=on
 fi
