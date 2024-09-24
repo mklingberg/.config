b6b953b1b9f0aabfdb74ef5e85aea69e293724e5
Marcus Klingberg <marcus.klingberg@gmail.com>
2024-09-24 22:51:22 +0200

    Showing airpods and case when airpods not available

--- sketchybar/plugins/airpods.sh
+++ sketchybar/plugins/airpods.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source "$HOME/.config/sketchybar/colors.sh"
 source "$HOME/.config/sketchybar/icons.sh"
 
 # airpods.sh
@@ -20,14 +21,14 @@ CASE_BATTERY=${CASE_BATTERY:-0}
 # Determine the lowest battery level
 LOWEST_BATTERY=$(( LEFT_BATTERY < RIGHT_BATTERY ? LEFT_BATTERY : RIGHT_BATTERY ))
 
-# Check if we have a value for case battery, then show it
-if [ "$CASE_BATTERY" -gt 0 ]; then
-    $BAR_NAME --set $NAME drawing=on label="$CASE_BATTERY%" icon=$ICON_AIRPODS_CASE
+# Check if we have a value for case battery, but not airpods then show it
+if [ "$CASE_BATTERY" -gt 0 ] && [ "$LOWEST_BATTERY" -eq 0 ] ; then
+    $BAR_NAME --set $NAME icon.font="$FONT_FACE:Medium:20.0" drawing=on label="$CASE_BATTERY%" icon=$ICON_AIRPODS_CASE
 # Check if LOWEST_BATTERY is empty (AirPods not connected)
 elif [ "$LOWEST_BATTERY" -eq 0 ]; then
     # Hide the item if LOWEST_BATTERY is empty
     $BAR_NAME --set $NAME drawing=off
 else
     # Show the item and set the label if LOWEST_BATTERY is not empty
-    $BAR_NAME --set $NAME drawing=on label="$LOWEST_BATTERY%" icon=$ICON_AIRPODS
+    $BAR_NAME --set $NAME icon.font="$FONT_FACE:Medium:16.0" drawing=on label="$LOWEST_BATTERY%" icon=$ICON_AIRPODS
 fi
