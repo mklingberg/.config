4180ef430f8128d9a0ae8b79d15bbd23a47426ac
Marcus Klingberg <marcus.klingberg@gmail.com>
2024-09-25 22:29:09 +0200

    Improved cpu load display with icons reflecting current load

--- sketchybar/plugins/cpu_load.sh
+++ sketchybar/plugins/cpu_load.sh
@@ -4,16 +4,21 @@ COLOR_CPU_MEDIUM=0xffed8796
 COLOR_CPU_HIGH=0xffff0000
 
 source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
+source "$HOME/.config/sketchybar/icons.sh"
 
 # Init default values
 MEDIUM_THRESHOLD=6.0
 HIGH_THRESHOLD=10.0
+
+ICON=$ICON_CPU_LOAD_1
 
 # Get the total number of CPU cores
 CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
 
 # Extract Load Avg values 
 LOAD_AVG=$(top -l 1 -n 0 | grep "Load Avg" | awk -F'[:,]' '{print $2}' | xargs)
+#LOAD_AVG=8.0
+
 
 # Determine the color based on CPU usage
 if (( $(echo "$LOAD_AVG > $HIGH_THRESHOLD" | bc -l) )); then
@@ -24,5 +29,28 @@ else
     ICON_COLOR=$COLOR_CPU_LOW  # Default color
 fi
 
+# Determine the icon based on CPU usage
+if (( $(echo "$LOAD_AVG > 12.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_10
+elif (( $(echo "$LOAD_AVG > 10.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_9
+elif (( $(echo "$LOAD_AVG > 9.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_8
+elif (( $(echo "$LOAD_AVG > 8.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_7
+elif (( $(echo "$LOAD_AVG > 7.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_6
+elif (( $(echo "$LOAD_AVG > 6.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_5
+elif (( $(echo "$LOAD_AVG > 5.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_4
+elif (( $(echo "$LOAD_AVG > 4.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_3
+elif (( $(echo "$LOAD_AVG > 3.0" | bc -l) )); then
+    ICON=$ICON_CPU_LOAD_2
+else
+    ICON=$ICON_CPU_LOAD_1
+fi
+
 # Update the bar with the CPU percentage and icon color
-$BAR_NAME --set $NAME icon.color=$ICON_COLOR
+$BAR_NAME --set $NAME icon=$ICON icon.color=$ICON_COLOR
