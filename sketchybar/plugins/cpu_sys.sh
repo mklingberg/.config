2d8c5f4d1bddcbac6ea42e8b189744e6e0ae83a1
Marcus Klingberg <marcus.klingberg@gmail.com>
2024-09-26 16:41:06 +0200

    Fixed issue with cpu user item not visible Increased load threshold before showing sys cpu usage.

--- sketchybar/plugins/cpu_sys.sh
+++ sketchybar/plugins/cpu_sys.sh
@@ -6,7 +6,7 @@ COLOR_CPU_HIGH=0xffff0000
 source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
 
 # Init default values
-LOAD_THRESHOLD=4.0
+LOAD_THRESHOLD=6.0
 
 MEDIUM_THRESHOLD=60
 HIGH_THRESHOLD=80
