e524820f4e8e1184f38ff9c84efe4a23a441f557
Marcus Klingberg <marcus.klingberg@gmail.com>
2024-09-26 21:46:19 +0200

    Adding idle icon and load avg evaluation for user cpu

--- sketchybar/plugins/cpu_user.sh
+++ sketchybar/plugins/cpu_user.sh
@@ -4,42 +4,55 @@ COLOR_CPU_MEDIUM=0xffed8796
 COLOR_CPU_HIGH=0xffff0000
 
 source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
+source "$HOME/.config/sketchybar/icons.sh"
 
 # Init default values
+LOAD_THRESHOLD=3.0
+
 MEDIUM_THRESHOLD=60
 HIGH_THRESHOLD=80
-LABEL_THRESHOLD=1
 SHOW_LABEL=false
+LABEL=""
+ICON=$ICON_CPU_IDLE
+COLOR=$COLOR_CPU_LOW
 
 # Get the total number of CPU cores
 CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
 
-# Extract user and system CPU percentages
-USER_CPU=$(echo "$CORE_COUNT" | top -l 1 -n 0 | grep "CPU usage" | awk -F'[:,%]' '{print $2}' | xargs)
+# Extract Load Avg values 
+LOAD_AVG=$(top -l 1 -n 0 | grep "Load Avg" | awk -F'[:,]' '{print $2}' | xargs)
 
-# Set locale to C for correct number interpretation
-export LC_NUMERIC=C
+# Check if the 1-minute load average is above the threshold
+if (( $(echo "$LOAD_AVG > $LOAD_THRESHOLD" | bc -l) )); then
+    # Extract user and system CPU percentages
+    USER_CPU=$(echo "$CORE_COUNT" | top -l 1 -n 0 | grep "CPU usage" | awk -F'[:,%]' '{print $2}' | xargs)
 
-# Calculate the CPU percentage
-# Round the values to zero decimals
-USER_CPU=$(printf "%.0f" "$USER_CPU")
+    # Set locale to C for correct number interpretation
+    export LC_NUMERIC=C
 
-# Determine the color based on CPU usage
-if [ $USER_CPU -gt $HIGH_THRESHOLD ]; then
-    ICON_COLOR=$COLOR_CPU_HIGH
-elif [ $USER_CPU -gt $MEDIUM_THRESHOLD ]; then
-    ICON_COLOR=$COLOR_CPU_MEDIUM
-else
-    ICON_COLOR=$COLOR_CPU_LOW  # Default color
-fi
+    # Calculate the CPU percentage
+    # Round the values to zero decimals
+    USER_CPU=$(printf "%.0f" "$USER_CPU")
 
-# Check if the label should be shown
-if [ $USER_CPU -gt $LABEL_THRESHOLD ]; then
+    # Determine the color based on CPU usage
+    if [ $USER_CPU -gt $HIGH_THRESHOLD ]; then
+        COLOR=$COLOR_CPU_HIGH
+    elif [ $USER_CPU -gt $MEDIUM_THRESHOLD ]; then
+        COLOR=$COLOR_CPU_MEDIUM
+    else
+        COLOR=$COLOR_CPU_LOW  # Default color
+    fi
+
     SHOW_LABEL=true
+    # Format the label
+    LABEL="${USER_CPU}%"
+    ICON=$ICON_CPU_USER
 fi
-
-# Format the label
-LABEL="${USER_CPU}%"
 
 # Update the bar with the CPU percentage and icon color
-$BAR_NAME --set $NAME label="$LABEL" label.drawing=$SHOW_LABEL label.color=$ICON_COLOR icon.color=$ICON_COLOR
+$BAR_NAME --set $NAME \
+        icon=$ICON \
+        label="$LABEL" \
+        label.drawing=$SHOW_LABEL \
+        label.color=$COLOR \
+        icon.color=$COLOR
