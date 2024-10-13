29d4eb89ce0baa4ce4b232e36d4a55fae7659ecd
Marcus Klingberg <marcus.klingberg@gmail.com>
2024-10-13 17:59:10 +0200

    Hide label if cate battery is full

--- sketchybar/plugins/airpods_case.sh
+++ sketchybar/plugins/airpods_case.sh
@@ -3,6 +3,8 @@ source "$HOME/.config/sketchybar/icons.sh"
 
 # airpods.sh
 # Fetch the left and right battery status of connected AirPods and update the bar label with the lowest value
+LABEL_THRESHOLD=95
+SHOW_LABEL=false
 
 # Fetch AirPods battery levels
 AIRPODS_INFO=$(system_profiler SPBluetoothDataType | grep -A 20 "AirPods")
@@ -14,7 +16,13 @@ CASE_BATTERY=${CASE_BATTERY:-0}
 
 # Check if we have a value for case battery, but not airpods then show it
 if [ "$CASE_BATTERY" -gt 0 ]; then
-    $BAR_NAME --set $NAME drawing=on label="$CASE_BATTERY%" icon=$ICON_AIRPODS_CASE
+
+    # Check if the case battery is below the threshold
+    if [ "$CASE_BATTERY" -lt "$LABEL_THRESHOLD" ]; then
+        SHOW_LABEL=true
+    fi
+
+    $BAR_NAME --set $NAME drawing=on label="$CASE_BATTERY%" label.drawing=$SHOW_LABEL icon=$ICON_AIRPODS_CASE
 else
     $BAR_NAME --set $NAME drawing=off
 fi
