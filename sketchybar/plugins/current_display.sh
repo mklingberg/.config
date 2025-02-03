4e208297d794492f743a9eb780d2ced3657f7c63
Marcus Klingberg <marcus.klingberg@gmail.com>
2025-02-03 19:55:28 +0100

    Remove unused plugin scripts for display, space, media, memory, and weather from sketchybar

--- sketchybar/plugins/current_display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the display number. Replace this with the actual command or script.
-#DISPLAY_NUMBER=$(your_command_to_get_display_number)
-
-# Set the item label to the display number.
-$BAR_NAME --set $NAME label="$INFO"
--- sketchybar/plugins/media.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STATE="$(echo "$INFO" | jq -r '.state')"
-if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-  $BAR_NAME --set $NAME label="$MEDIA" drawing=on
-else
-  $BAR_NAME --set $NAME drawing=off
-fi
