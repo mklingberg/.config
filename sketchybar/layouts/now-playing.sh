b68e4813a92a96b8527bef99cae87f4be7d07b4b
Marcus Klingberg <marcus.klingberg@gmail.com>
2025-02-17 13:13:17 +0100

    Adding icon with player state

--- sketchybar/layouts/now-playing.sh
+++ sketchybar/layouts/now-playing.sh
@@ -9,32 +9,31 @@ ITEM_DIR="$CONFIG_DIR/items"
 SPOTIFY_EVENT="com.spotify.client.PlaybackStateChanged"
 
 BAR_HEIGHT=300
+MARGIN_RIGHT=20
+MARGIN_BOTTOM=20
 
 bar=(
     height=$BAR_HEIGHT
     color=$TRANSPARENT
-    #width=200
-    #margin=10
-    #padding_left=8
-    #padding_right=20
-    display=1
+    display=main
     topmost=on
     position=bottom
-    y_offset=10
+    y_offset=$MARGIN_BOTTOM
 )
 
 default=(
     background.height=$BAR_HEIGHT
-    #icon.color=$COLOR_TEXT
-    #icon.font="$FONT_FACE:Medium:16.0"
-    #icon.padding_left=0
-    #icon.padding_right=0
-    #icon.y_offset=0
-    #label.color=$COLOR_TEXT
-    #label.font="$FONT_FACE:Medium:14.0"
-    #label.y_offset=1
-    #label.padding_left=0
-    #label.padding_right=0
+)
+
+icon=(
+    icon.width=200
+    background.padding_right=-200
+    icon.color=0xaaffffff
+    icon.y_offset=72
+    icon.align=right
+    icon.padding_right=8
+    icon=$ICON_SPOTIFY
+    icon.font.size=40
 )
 
 now_playing_thumbnail=(
@@ -81,6 +80,10 @@ now_playing_track=(
 $BAR_NAME \
     --bar "${bar[@]}" \
     --default "${default[@]}" \
+    --add item spacer_outer right \
+    --set spacer_outer \
+          width=$MARGIN_RIGHT \
+          label.drawing=off \
     --add event spotify_change $SPOTIFY_EVENT \
     --add item spacer_1 right \
     --set spacer_1 \
@@ -93,12 +96,15 @@ $BAR_NAME \
     --set now_playing.artist "${now_playing_artist[@]}" \
     --add item now_playing.track right \
     --set now_playing.track "${now_playing_track[@]}" \
+    --add item icon right \
+    --set icon "${icon[@]}" \
     --add item spacer_2 right \
     --set spacer_2 \
-          width=8 \
+          width=6 \
           label.drawing=off \
     --add bracket now_playing.bracket \
             spacer_1 \
+            icon \
             now_playing.thumbnail \
             now_playing.artist \
             now_playing.track \
--- sketchybar/plugins/now_playing.sh
+++ sketchybar/plugins/now_playing.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source "$HOME/.config/$BAR_NAME/theme.sh"
+
 # Spotify JSON / $INFO comes in malformed, line below sanitizes it
 SPOTIFY_JSON="$INFO"
 
@@ -7,11 +9,24 @@ update_track() {
     PLAYER_STATE=$(echo "$SPOTIFY_JSON" | jq -r '.["Player State"]')
     TRACK="$(echo "$SPOTIFY_JSON" | jq -r .Name)"
     ARTIST="$(echo "$SPOTIFY_JSON" | jq -r .Artist)"
+    ICON=$ICON_SPOTIFY
+
+    if [ "$PLAYER_STATE" = "Playing" ]; then
+        ICON=$ICON_SPOTIFY_PLAY
+    elif [ "$PLAYER_STATE" = "Paused" ]; then
+        ICON=$ICON_SPOTIFY_PAUSE
+    elif [ "$PLAYER_STATE" = "Stopped" ]; then
+        ICON=$ICON_SPOTIFY_PAUSE
+    else
+        ICON=$ICON_SPOTIFY
+    fi
 
     $BAR_NAME \
         --set now_playing.artist label="$ARTIST" \
-        --set now_playing.track label="$TRACK"
+        --set now_playing.track label="$TRACK" \
+        --set icon icon=$ICON
 }
+
 
 case "$SENDER" in
 "mouse.clicked")
